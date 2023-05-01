e844a56f87b696bc8cd36ce47be909b2236011e3
abc <abc.com>
2023-05-02 00:31:42 +0500

    4

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-console/log("abc");
 app.get("/", (req, res) => {
     app.use(express.static(path.resolve(__dirname,"client","build")));
     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
