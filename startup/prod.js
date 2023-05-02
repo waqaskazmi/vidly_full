fd5bad9c29a8b92db7b1470ed3af7b43197ccba5
abc <abc.com>
2023-05-02 18:52:55 +0500

    12

--- startup/prod.js
+++ startup/prod.js
@@ -2,6 +2,13 @@ const helmet = require('helmet');
 const compression = require('compression');
 
 module.exports = function(app){
-    app.use(helmet());
+    app.use(helmet({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: ["'self'"],
+            connectSrc: ["'self'", 'http://127.0.0.1:8000', 'ws://localhost:42877/']
+          }
+        }
+      }));
     app.use(compression());
 }
