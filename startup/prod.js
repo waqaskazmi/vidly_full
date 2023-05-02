83aaa7dfc1cd302d568cfdfc44dc1774304990f1
abc <abc.com>
2023-05-02 19:04:13 +0500

    14

--- startup/prod.js
+++ startup/prod.js
@@ -1,14 +1,14 @@
-//const helmet = require('helmet');
+const helmet = require('helmet');
 const compression = require('compression');
 
 module.exports = function(app){
-    // app.use(helmet({
-    //     contentSecurityPolicy: {
-    //       directives: {
-    //         defaultSrc: ["'self'"],
-    //         connectSrc: ["'self'", 'http://127.0.0.1:8000', 'ws://localhost:42877/']
-    //       }
-    //     }
-    //   }));
+    app.use(helmet({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: ["'self'"],
+            connectSrc: ["'self'", 'http://127.0.0.1:8000', 'ws://localhost:42877/']
+          }
+        }
+      }));
     app.use(compression());
 }
